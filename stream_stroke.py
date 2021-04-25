1c7790b4c68d0ed083b0f959af674f84f351b77a
PrethushThomas <82256003+PrethushThomas@users.noreply.github.com>
2021-04-25 17:48:01 +0530

    Update stream_stroke.py

--- stream_stroke.py
+++ stream_stroke.py
@@ -17,8 +17,8 @@ def predict(model, input_df):
 
 def run():
     from PIL import Image
-    image = Image.open('/content/brain_stroke.jpg')
-    image_office = Image.open('/content/stroke_recovery.jpg')
+    image = Image.open('brain_stroke.jpg')
+    image_office = Image.open('stroke_recovery.jpg')
     st.image(image,use_column_width=True)
     add_selectbox = st.sidebar.selectbox(
     "How would you like to predict?",
